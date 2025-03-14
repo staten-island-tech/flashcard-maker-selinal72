3535f250d67a66989a800d6f4529a126729a3796
selinal72 <selinal72@nycstudents.net>
2025-03-14 10:14:29 -0400

    started

---
+++ flash.py
@@ -0,0 +1,6 @@
+def teacher_mode(): 
+    class flashcards:
+        def _init_(card, phrase, answer):
+            card.phrase = int("Word/Phrase: ")
+            card.answer = int("Definition/Answer: ")
+        def create 
