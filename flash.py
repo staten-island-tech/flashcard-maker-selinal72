22dd638e1930c42dc48deebb4595dcc3e7faef7d
selinal72 <selinal72@nycstudents.net>
2025-03-25 10:15:52 -0400

    working

--- flash.py
+++ flash.py
@@ -9,25 +9,32 @@ class flashcards:
     def __init__(card, phrase, answer):
         card.phrase = phrase
         card.answer = answer
-    def display(card):
+    def create_dict(card):
         print(card.phrase, card.answer)
+        x = {card.phrase : card.answer}
+        return x
+
 
 ask = input("Create flashcard? Y/N ")
 while ask == "Y":
     input1 = input("Enter Word/Phrase: ")
     input2 = input("Enter Definition/Answer: ")
     mansa_musa = flashcards(input1, input2)
-    mansa_musa.display()
+    print(mansa_musa.create_dict())
     cards_data.append(mansa_musa.__dict__)
     print(cards_data)
     ask = input("Create flashcard? Y/N ")
+    print(cards_data)
 
 class student:
     def __init__(self, name, score):
         self.name = name
         self.score = score
-    def
+    def points(self):
+        self.score =+ 1
+
+selina = student("selina", 0)
 switch = input("Switch to Student Mode? Y/N ")
 while switch == "Y":
-    for x,y in cards_data.items():
-        print
+    phrases = cards_data['phrase']
+    print(phrases)
